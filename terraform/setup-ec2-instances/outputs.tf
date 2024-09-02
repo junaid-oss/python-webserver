9a049fe605c3733289fa71a674cf3bde5543d97c
Junaid <Junaid.habeeb@gmail.com>
2024-09-02 18:54:59 +0530

    destroy

--- terraform/setup-ec2-instances/outputs.tf
+++ terraform/setup-ec2-instances/outputs.tf
@@ -1,9 +1,9 @@
-output "instance_id" {
-  description = "ID of the created EC2 instance"
-  value       = aws_instance.my-ec2.id
-}
+#output "instance_id" {
+#  description = "ID of the created EC2 instance"
+#  value       = aws_instance.my-ec2.id
+#}
 
-output "instance_public_ip" {
-  description = "Public IP of the created EC2 instance"
-  value       = aws_instance.my-ec2.public_ip
-}
+#output "instance_public_ip" {
+#  description = "Public IP of the created EC2 instance"
+#  value       = aws_instance.my-ec2.public_ip
+#}
