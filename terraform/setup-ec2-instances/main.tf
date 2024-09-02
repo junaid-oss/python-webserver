b4dfad255cebfe2c3936d9028f057ba415781bb9
Junaid <Junaid.habeeb@gmail.com>
2024-09-02 18:35:06 +0530

    destroy ec2

--- terraform/setup-ec2-instances/main.tf
+++ terraform/setup-ec2-instances/main.tf
@@ -1,13 +1,13 @@
-resource "aws_instance" "my-ec2" {
-  ami           = "ami-090abff6ae1141d7d"  # Example AMI ID (Amazon Linux 2 in us-east-1)
-  instance_type = "t3.micro"
+#resource "aws_instance" "my-ec2" {
+#  ami           = "ami-090abff6ae1141d7d"  # Example AMI ID (Amazon Linux 2 in us-east-1)
+#  instance_type = "t3.micro"
 
-  tags = {
-    Name = "junaid"
-  }
+#  tags = {
+#    Name = "junaid"
+#  }
 
   # Optionally specify key_name if you want to use SSH access
-  key_name = "ssh-keys-ec2"
+#  key_name = "ssh-keys-ec2"
 
   # Optionally specify user_data for initial setup
   # user_data = <<-EOF
@@ -17,4 +17,4 @@ resource "aws_instance" "my-ec2" {
   #               systemctl start httpd
   #               systemctl enable httpd
   #               EOF
-}
+#}
