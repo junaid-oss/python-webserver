bb70780b29ab3beff7f80615a7818c044907cc62
Junaid <Junaid.habeeb@gmail.com>
2024-09-02 18:18:35 +0530

    new added code

--- terraform/setup-ec2-instances/outputs.tf
+++ terraform/setup-ec2-instances/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
   description = "ID of the created EC2 instance"
-  value       = aws_instance.example.id
+  value       = aws_instance.my-ec2.id
 }
 
 output "instance_public_ip" {
   description = "Public IP of the created EC2 instance"
-  value       = aws_instance.example.public_ip
+  value       = aws_instance.my-ec2.public_ip
 }
