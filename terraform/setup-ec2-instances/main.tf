a72833515d3567f091cd264d99e9cf84e546620b
Junaid <Junaid.habeeb@gmail.com>
2024-09-02 16:51:34 +0530

    ameding the existing commit added ec2 instance creation login to terraform

---
+++ terraform/setup-ec2-instances/main.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "my-ec2" {
+  ami           = "ami-090abff6ae1141d7d"  # Example AMI ID (Amazon Linux 2 in us-east-1)
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "junaid"
+  }
+
+  # Optionally specify key_name if you want to use SSH access
+  key_name = "ssh-keys-ec2"
+
+  # Optionally specify user_data for initial setup
+  # user_data = <<-EOF
+  #               #!/bin/bash
+  #               yum update -y
+  #               yum install -y httpd
+  #               systemctl start httpd
+  #               systemctl enable httpd
+  #               EOF
+}
